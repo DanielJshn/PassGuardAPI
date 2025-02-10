d7501311d3f6a36a7a4c4d5c070390ed74c32015
Daniel Dobosh <danieldobosh362@gmail.com>
2025-02-10 16:09:27 +0200

    Rename Columns

---
+++ Migrations/20250210134206_RenameTitleToUserName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace V2iSafe.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTitleToUserName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "title",
+                table: "Password",
+                newName: "userName");
+
+            migrationBuilder.RenameColumn(
+                name: "organization",
+                table: "Password",
+                newName: "title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "userName",
+                table: "Password",
+                newName: "title");
+
+            migrationBuilder.RenameColumn(
+                name: "title",
+                table: "Password",
+                newName: "organization");
+        }
+
+    }
+}
--- Models/Password/Password.cs
+++ Models/Password/Password.cs
@@ -6,9 +6,9 @@ namespace apief
         public Guid id { get; set; }
         public string? categoryId { get; set; }
         public string? password { get; set; }
-        public string? organization { get; set; }
-        public string? organizationLogo { get; set; }
         public string? title { get; set; }
+        public string? organizationLogo { get; set; }
+        public string? userName { get; set; }
         public string? createdTime { get; set; } = null;
         public string? modifiedTime { get; set; } = null;
         public List<AdditionalField> additionalFields { get; set; } = new List<AdditionalField>();

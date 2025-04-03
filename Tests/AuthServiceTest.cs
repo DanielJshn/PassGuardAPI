5115e4964d4ccdcb6cb37f4cd2861edd35a8f28d
Daniel Dobosh <danieldobosh362@gmail.com>
2025-04-03 14:24:13 +0300

    Add tests for CreateNewAccountAsync

--- Tests/AuthServiceTest.cs
+++ Tests/AuthServiceTest.cs
@@ -19,10 +19,10 @@ namespace apief
             _userService = new AuthService(_authRepositoryMock.Object, _mapperMock.Object, _loggerMock.Object);
         }
 
+
         [Fact]
         public async Task CreateNewAccountAsync_ShouldCreateUserSuccessfully()
         {
-            // Arrange
             var userDto = new UserDataRegistrationDto
             {
                 email = "test@example.com",
@@ -37,13 +37,73 @@ namespace apief
             _mapperMock.Setup(m => m.Map<UserData>(userDto)).Returns(userModel);
             _authRepositoryMock.Setup(r => r.AddUserDataAsync(It.IsAny<UserData>())).Returns(Task.CompletedTask);
 
-            // Act
             var result = await _userService.CreateNewAccountAsync(userDto);
 
-            // Assert
             Assert.NotNull(result);
             Assert.NotEqual(Guid.Empty, result.id);
             _authRepositoryMock.Verify(r => r.AddUserDataAsync(It.IsAny<UserData>()), Times.Once);
+        }
+
+
+        [Fact]
+        public async Task CreateNewAccountAsync_ShouldThrowException_WhenEmailIsMissing()
+        {
+            var userDto = new UserDataRegistrationDto
+            {
+                email = "",
+                hashedPK = "hashedPK",
+                hashedPKSalt = "hashedPKSalt",
+                encryptedSK = "encryptedSK",
+                hashedRK = "hashedRK",
+                recoverySK = "recoverySK"
+            };
+
+            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _userService.CreateNewAccountAsync(userDto));
+            Assert.Contains("Email is required.", exception.Message);
+        }
+
+
+        [Fact]
+        public async Task CreateNewAccountAsync_ShouldThrowException_WhenMultipleFieldsAreMissing()
+        {
+            var userDto = new UserDataRegistrationDto
+            {
+                email = "",
+                hashedPK = "",
+                hashedPKSalt = "",
+                encryptedSK = "",
+                hashedRK = "",
+                recoverySK = ""
+            };
+
+            string expectedMessage = "Email is required. HashedPK is required. HashedPKSalt is required. EncryptedSK is required. HashedRK is required. RecoverySK is required.";
+
+            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _userService.CreateNewAccountAsync(userDto));
+
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+
+        [Fact]
+        public async Task CreateNewAccountAsync_ShouldThrowException_WhenDatabaseFails()
+        {
+            var userDto = new UserDataRegistrationDto
+            {
+                email = "test@example.com",
+                hashedPK = "hashedPK",
+                hashedPKSalt = "hashedPKSalt",
+                encryptedSK = "encryptedSK",
+                hashedRK = "hashedRK",
+                recoverySK = "recoverySK"
+            };
+
+            _mapperMock.Setup(m => m.Map<UserData>(userDto)).Returns(new UserData());
+
+            _authRepositoryMock.Setup(r => r.AddUserDataAsync(It.IsAny<UserData>())).ThrowsAsync(new Exception("DB error"));
+
+            var exception = await Assert.ThrowsAsync<Exception>(() => _userService.CreateNewAccountAsync(userDto));
+
+            Assert.Equal("An error occurred while saving the user data.", exception.Message);
         }
     }
 }

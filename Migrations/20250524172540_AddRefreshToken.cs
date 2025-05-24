c9fb346088d3fdb670602f6c3463f15beb0e6ac2
Daniel Dobosh <danieldobosh362@gmail.com>
2025-05-24 20:45:49 +0300

    Add Refresh fields in UserData

---
+++ Migrations/20250524172540_AddRefreshToken.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace V2iSafe.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                schema: "Dbo",
+                table: "UserData",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiration",
+                schema: "Dbo",
+                table: "UserData",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                schema: "Dbo",
+                table: "UserData");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiration",
+                schema: "Dbo",
+                table: "UserData");
+        }
+    }
+}
